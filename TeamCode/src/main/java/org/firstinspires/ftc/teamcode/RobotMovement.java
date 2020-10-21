9623248e80ac847d72aeba216cb7a3f7e45ece27
Masenaters <71571359+Masenaters@users.noreply.github.com>
2020-10-20 17:04:05 -0700

    Added Motor Var

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMovement.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class RobotMovement {
+    DcMotor leftBack;
+    DcMotor leftFront;
+    DcMotor rightBack;
+    DcMotor rightFront;
+    public void runOpMode(){
+
+    }
 }
