8a807a1e7012615e889bd51e8d4b73f246befec9
Masenaters <71571359+Masenaters@users.noreply.github.com>
2020-10-21 20:17:02 -0700

    Added Init void

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMovement.java
@@ -1,13 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class RobotMovement {
     DcMotor leftBack;
     DcMotor leftFront;
     DcMotor rightBack;
     DcMotor rightFront;
-    public void runOpMode(){
 
+    public void init(){
+        HardwareMap hardwareMap;
+        leftBack = hardwareMap.dcMotor.get("");
+        leftFront = hardwareMap.dcMotor.get("");
+        rightBack = hardwareMap.dcMotor.get("");
+        rightFront = hardwareMap.dcMotor.get("");
+        
     }
 }
