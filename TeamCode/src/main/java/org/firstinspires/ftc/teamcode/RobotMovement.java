4d49d9161a98b1e4e2128cb519c3d350d4fd11b3
Masenaters <71571359+Masenaters@users.noreply.github.com>
2020-10-21 20:47:57 -0700

    Added Power, Implements, new voids
    I added some power for the motors in the loop void but the power is not set yet. I also added more implements from a sample for most of the functions we would use

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMovement.java
@@ -1,20 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-public class RobotMovement {
+@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
+@Disabled
+public class RobotMovement extends OpMode {
+
+    ElapsedTime runtime = new ElapsedTime();
     DcMotor leftBack;
     DcMotor leftFront;
     DcMotor rightBack;
     DcMotor rightFront;
 
+    @Override
     public void init(){
-        HardwareMap hardwareMap;
+        HardwareMap hardwareMap = null;
         leftBack = hardwareMap.dcMotor.get("");
         leftFront = hardwareMap.dcMotor.get("");
         rightBack = hardwareMap.dcMotor.get("");
         rightFront = hardwareMap.dcMotor.get("");
         
+    }
+
+    @Override
+    public void loop() {
+        leftBack.setPower(0.7);
+        leftFront.setPower(0.7);
+        leftBack.setPower(0.7);
+        rightFront.setPower(0.7);
     }
 }
